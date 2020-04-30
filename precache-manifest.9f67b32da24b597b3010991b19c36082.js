77ed27373e684566b0f26a0e80b2671e3222c5ac
Alexei Bostan <alexei-bostan@dedagroup.it>
2020-04-30 22:26:05 +0200

    Updates

---
+++ precache-manifest.9f67b32da24b597b3010991b19c36082.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54ab82a4fec021209d2f977d73c6733f",
+    "url": "/ab-portfolio/index.html"
+  },
+  {
+    "revision": "2cd8f54aceee74146522",
+    "url": "/ab-portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "02eee512b54f3eda37e3",
+    "url": "/ab-portfolio/static/js/2.f3e4d052.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/ab-portfolio/static/js/2.f3e4d052.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cd8f54aceee74146522",
+    "url": "/ab-portfolio/static/js/main.ed16e0e0.chunk.js"
+  },
+  {
+    "revision": "91e1834ea09776f79654",
+    "url": "/ab-portfolio/static/js/runtime-main.a2359473.js"
+  }
+]);
